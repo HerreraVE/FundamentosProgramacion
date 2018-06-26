688de7d8c688e1185870ad1e03f324f5b11123e3
pasthortown <pastorchessmaster@gmail.com>
2018-06-26 08:51:02 -0500

    ok

--- programacion3/src/pages/home/home.ts
+++ programacion3/src/pages/home/home.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, NgModule } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { } from '@types/googlemaps';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  ngOnInit() {
+    this.startGoogleMap();
+  }
+
+  startGoogleMap() {
+    const mapProp = {
+        center: new google.maps.LatLng(-0.224710, -78.516763),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
 }
